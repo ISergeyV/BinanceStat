83e4f53085e1f84f4740368d6ddbf994c9356e16
Sergey Isayevskiy <isayevskiy@gmail.com>
2021-03-18 22:59:29 +0200

    change authorization system

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ require __DIR__ . '/vendor/autoload.php';
 // 2. config in specified file
 //$api = new Binance\API( "somefile.json" );
 // 3. config by specifying api key and secret
-$api = new Binance\API("GvqTrkRj7zNtc0TkNCsmg7EwjwgPuPZB6Wb1B8DbQg2IMHvV5uV4Lfv6sw0Hhtvd", "P9wzexYLOQwvonZEa01YHAQB9S7E2Dy4wuK4RpXBRp7EKoN4irocqojg8r2i51BF");
+$key = include __DIR__ . "key.php";
+$api = new Binance\API($key[0], $key[1]);
 $currencyGet = 'BUSD';
 if (isset($_GET["currency"])) {
     $currencyGet = strtoupper(htmlspecialchars($_GET["currency"]));
--- key.php
+++ key.php
@@ -1,2 +1,1 @@
-<?php
-return ["GvqTrkRj7zNtc0TkNCsmg7EwjwgPuPZB6Wb1B8DbQg2IMHvV5uV4Lfv6sw0Hhtvd", "P9wzexYLOQwvonZEa01YHAQB9S7E2Dy4wuK4RpXBRp7EKoN4irocqojg8r2i51BF"];
+<?php return ["GvqTrkRj7zNtc0TkNCsmg7EwjwgPuPZB6Wb1B8DbQg2IMHvV5uV4Lfv6sw0Hhtvd", "P9wzexYLOQwvonZEa01YHAQB9S7E2Dy4wuK4RpXBRp7EKoN4irocqojg8r2i51BF"];
